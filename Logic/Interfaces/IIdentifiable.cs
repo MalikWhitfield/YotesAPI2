ff27bf4248ba22e55b98ae90722f9c05f9bc91d3
MalikWhitfield <malik.whitfield@associa.us>
2021-02-22 16:18:49 -0500

    add logic/data/packages

---
+++ Logic/Interfaces/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Interfaces
+{
+    public interface IIdentifiable<K>
+    {
+        K Id { get; set; }
+    }
+}
---
+++ Logic/Interfaces/ITrackable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Interfaces
+{
+    public interface ITrackable
+    {
+        DateTime CreatedAt { get; set; }
+        string CreatedBy { get; set; }
+        DateTime ModifiedAt { get; set; }
+        string ModifiedBy { get; set; }
+    }
+}
+
