43d06a010c8187968ca6432bebacb6ad95538d30
MalikWhitfield <malik.whitfield@associa.us>
2021-02-23 16:04:31 -0500

    add user service and model

---
+++ Common/DataModels/ListResults.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Common.DataModels
+{
+    public class ListResults<T>
+    {
+        public ListResults() { }
+
+        public IEnumerable<T> Results { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
